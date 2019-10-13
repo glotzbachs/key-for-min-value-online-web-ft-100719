daabcbde1ba95f718ed0ef4affdd5ec4a1eb8d01
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-13 20:57:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,8 @@ def key_for_min_value(name_hash)
   name_hash.each{|key, value| 
     if value<lowest_value
       lowest_value=value
+      lowest_key=key
     end
   }
-  lowest_value
+  lowest_key
 end
